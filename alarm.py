c8f8195511f5096ffd4e71d6c6b54c701b1fa1e9
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-01-06 16:05:35 +0100

    Now display message when time is correct

--- alarm.py
+++ alarm.py
@@ -1,22 +1,37 @@
-time = (12,15,30)
+import time
+
+clock = (12,15,30)
 #format = "AM"
 
-
-#1/ set an alarm hour
-#2/ act when it's the said hour
-#3/
-
+clock_display = str(clock) #+ " " + clock[1] + " " + clock[2] + " "
+print(clock_display)
 
 def set_alarm():
     user_alarm = input() #return tuple (hh,mm,ss)
     return user_alarm
 
-alarm_time = (11,15,30) #set_alarm()
+alarm_time = (12,15,45) #set_alarm()
 
-def display_alarm(time, alarm_time):
-    if time == alarm_time:
-        print("Ring ring! Ring ring!")
-    #if time[1] = alarm_time[1] + 10
+def display_alarm(clock, alarm_time):
+    if clock == alarm_time:
+        return "Ring ring! Ring ring!"
+    else:
+        return ""
+        #while clock[1] < alarm_time[1] - 10:
+            #if clock[2] % 2 == 0:
+                #print("Ring ring! Ring ring!")
+            #else:
+                #print("", end="\r")   
+    #if clock[1] = alarm_time[1] + 10
         #unprint?
 
-display_alarm(time, alarm_time)
+#display_alarm(clock, alarm_time)
+
+
+while (True):
+    print(clock_display, end='\r')
+    temp = clock[2]+1
+    clock = (12,15,temp)
+    clock_display = str(clock)
+    clock_display += display_alarm(clock, alarm_time)
+    time.sleep(1)
