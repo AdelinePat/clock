b6b9e75a2dbd76fe25c47c1ecfb11470b941b4a9
enolaworld <enolaworld@gmail.com>
2025-01-06 20:08:24 +0100

    Ajout des fichiers main.py et ihm.py

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ def set_time():
     second = int(input("Enter the seconds (0-59: "))
     time = (hour, minute, second)
     return time
-def choose_alarm():
-    alarm_choice = input("Do you want to set an alarm ?(yes/no):").strip.lower()
-    return alarm_choice
-alarm = None
-alarm_choice = choose_alarm()
-if alarm_choice == "yes":
-    return set_alarm()
 
 def choose_format():
     """ Ask the user to choose between 12h or 24h format """
     format_choice = int(input("Choose the time format (12h/24h): "))
     return format_choice
+
+def choose_alarm():
+    alarm_choice = input("Do you want to set an alarm ?(yes/no):").strip.lower()
+    return alarm_choice
+
+alarm = None
+alarm_choice = choose_alarm()
+if alarm_choice == "yes":
+    set_alarm()
+
+
 
     
 
