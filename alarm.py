0033e1cb186ef11e2358339d6a5ff6cca294ca6d
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-01-07 15:08:16 +0100

    Removed int for tuple and added a wait for the pause function

--- alarm.py
+++ alarm.py
@@ -1,10 +1,10 @@
 import time
 
 def set_alarm():
-    alarm_hours = int(input("Enter alarm hours  (0-23): "))
-    alarm_minutes = int(input("Enter alarm minutes (0-59): "))
-    alarm_seconds = int(input("Enter alarm seconds (0-59): ")) 
-    user_alarm = (alarm_hours, alarm_minutes, alarm_seconds)
+    alarm_hours = input("Enter alarm hours  (0-23): ")
+    alarm_minutes = input("Enter alarm minutes (0-59): ")
+    alarm_seconds = input("Enter alarm seconds (0-59): ") 
+    user_alarm = (alarm_hours, alarm_minutes, alarm_seconds) # alarm_hours + ":" + alarm_minutes + ":" + alarm_seconds
     return user_alarm #return tuple (hh,mm,ss) ; alarm = set_alarm()
 
 # In display_time: current_time += display_alarm(clock, alarm)
