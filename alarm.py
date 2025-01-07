b723e1651aac949519cbd2ea5839c5a165e93528
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-01-07 09:07:59 +0100

    Commented test values

--- alarm.py
+++ alarm.py
@@ -1,22 +1,27 @@
 import time
 
-clock = (12,15,30)
+# Test values
+#clock = (12,15,30)
 #format = "AM"
-
-clock_display = str(clock) #+ " " + clock[1] + " " + clock[2] + " "
-print(clock_display)
+#clock_display = str(clock) #+ " " + clock[1] + " " + clock[2] + " "
+#print(clock_display)
 
 def set_alarm():
     user_alarm = input() #return tuple (hh,mm,ss)
     return user_alarm
 
-alarm_time = (12,15,45) #set_alarm()
+alarm_time = set_alarm() #(12,15,45) 
 
 def display_alarm(clock, alarm_time):
     if clock == alarm_time:
         return "Ring ring! Ring ring!"
     else:
         return ""
+
+
+
+
+
         #while clock[1] < alarm_time[1] - 10:
             #if clock[2] % 2 == 0:
                 #print("Ring ring! Ring ring!")
@@ -28,10 +33,10 @@ def display_alarm(clock, alarm_time):
 #display_alarm(clock, alarm_time)
 
 
-while (True):
-    print(clock_display, end='\r')
-    temp = clock[2]+1
-    clock = (12,15,temp)
-    clock_display = str(clock)
-    clock_display += display_alarm(clock, alarm_time)
-    time.sleep(1)
+#while (True):
+#    print(clock_display, end='\r')
+#    temp = clock[2]+1
+#    clock = (12,15,temp)
+#    clock_display = str(clock)
+#    clock_display += display_alarm(clock, alarm_time)
+#    time.sleep(1)
