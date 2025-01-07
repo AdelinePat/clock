ec2b6398d4fc2f55e77aa0d77d82e957ec68f2c8
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-01-07 15:08:16 +0100

    Removed int for tuple and added a wait for the pause function

--- pause.py
+++ pause.py
@@ -2,16 +2,17 @@
 import keyboard
 import time
 
-def main():
-    clock = (1,2,3)
-    while True:
-        time.sleep(1)
-        clock = (1,2,clock[2]+1)
-        print(clock, end="\r")
-        try:
-            if keyboard.is_pressed('a'):
-                print(clock, end='\r')
-        except:
-            print("error")
+#def main():
+clock = (1,2,3)
+while True:
+    time.sleep(1)
+    clock = (1,2,clock[2]+1)
+    print(clock, end="\r")
+    try:
+        if keyboard.is_pressed('a'):
+            keyboard.wait('b')
+    except:
+        print("error")
+    #time.sleep(1)
 
-main()
+#main()
