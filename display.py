18ff9f28745c5afd89fd6e325cd8aa67cc7acfa0
joseph-mangeot <jolyne.mangeot@gmail.com>
2025-01-08 11:41:07 +0100

    terminal display clean and addition of style variables

--- display.py
+++ display.py
@@ -1,137 +1,146 @@
 # Import des librairies nécessaires
 import datetime, time, threading
 
-jour = ('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi')
-mois = (None, 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre')
+day = ('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi')
+month = (None, 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre')
 
 stop = 0
 mode = 1
-unité = ["l'heure", "l'heure", "les minutes", "les secondes", "AM / PM"]
-unité_b = [11,11,7,3]
-unité_max = [12,23,60,60]
-alarme = ["__","__","__","__","__"]
+unit = ["l'heure", "l'heure", "les minutes", "les secondes", "AM / PM"]
+unit_b = [11,11,7,3]
+unit_max = [12,23,60,60]
+alarm = ["__","__","__","__","__"]
 
-ansi_line1 = "\033[s\033[H\033[2K"
+
+cursor_line_1 = "\033[s\033[H\033[2K"
+cursor_line_2 = "\033[s\033[H\033[1B\033[2K"
+cursor_line_3 = "\033[H\033[2B\033[2K"
+cursor_line_4 = "\033[H\033[3B\033[2K"
+cursor_line_5 = "\033[H\033[4B\033[2K"
+cursor_line_6 = "\033[H\033[5B\033[2K"
+
+cursor_load = "\033[u"
+cursor_linecls = "\033[2K"
+cursor_lightcls = "\033[0J"
+cursor_heavycls = "\033[H\033[0J"
+
+cursor_bold_start = "\033[1m"
+cursor_bold_end = "\033[22m"
+cursor_style_finish = "\033[0m"
 
 # Définition des fonctions clés
-def afficher_date():
-    calendrier = (jour[int(datetime.datetime.now().strftime("%w"))], datetime.datetime.now().strftime("%d"), mois[int(datetime.datetime.now().strftime("%m"))])
-    return calendrier
+def calendar():
+    calendar = (day[int(datetime.datetime.now().strftime("%w"))], datetime.datetime.now().strftime("%d"), month[int(datetime.datetime.now().strftime("%m"))])
+    return calendar
 
-def afficher_heure():
-    horloge = (datetime.datetime.now().strftime("%I"), datetime.datetime.now().strftime("%H"), datetime.datetime.now().strftime("%M"), datetime.datetime.now().strftime("%S"), datetime.datetime.now().strftime("%p"))
-    return horloge
+def clock():
+    datetime_now_clock = (datetime.datetime.now().strftime("%I"), datetime.datetime.now().strftime("%H"), datetime.datetime.now().strftime("%M"), datetime.datetime.now().strftime("%S"), datetime.datetime.now().strftime("%p"))
+    return datetime_now_clock
 
-def afficher_alarme():
-    if alarme[0] != "__" and mode%2==1:
-        print(f"\033[s\033[H\n\033[2K! Alarme définie pour {alarme[1]}:{alarme[2]}:{alarme[3]} !\033[u", end="", flush=True)
-    elif alarme[0] != "__" and mode%2==0:
-        print(f"\033[s\033[H\n\033[2K! Alarme définie pour {alarme[0]}:{alarme[2]}:{alarme[3]} {alarme[4]} !\033[u", end="", flush=True)
-    else: print(f"\033[s\033[H\n\033[2KAucune alarme définie.\033[u", end="", flush=True)
+def display_alarm():
+    global alarm
+    while True:
+        if alarm[0] != "__" and mode%2==1:
+            print(f"{cursor_line_2}! Alarme définie pour {alarm[1]}:{alarm[2]}:{alarm[3]} !{cursor_load}", end="", flush=True)
+        elif alarm[0] != "__" and mode%2==0:
+            print(f"{cursor_line_2}! Alarme définie pour {alarm[0]}:{alarm[2]}:{alarm[3]} {alarm[4]} !{cursor_load}", end="", flush=True)
+        elif alarm[0] != "__" and clock()[int(mode%2)] == alarm[int(mode%2)] and clock()[2] == alarm[2] and clock()[3] == alarm[3]:
+                print(f"{cursor_line_2}RINGGGGG{cursor_load}", end="", flush=True)
+                time.sleep(5)
+                alarm = ["__","__","__","__","__"]
+        else: print(f"{cursor_line_2}Aucune alarme définie.{cursor_load}", end="", flush=True)
+        time.sleep(0.5)
+        if stop == 1:
+            return
 
 
-def affichage():
+def display_clock():
     while True:
         if mode%2 == 0:
-            print(f"{ansi_line1}{afficher_date()[0]} {afficher_date()[1]} {afficher_date()[2]}   \
-{afficher_heure()[0]} : {afficher_heure()[2]} : {afficher_heure()[3]} {afficher_heure()[4]}\033[u", end="", flush=True)
+            print(f"{cursor_line_1}{calendar()[0]} {calendar()[1]} {calendar()[2]}   \
+{clock()[0]} : {clock()[2]} : {clock()[3]} {clock()[4]}{cursor_load}", end="", flush=True)
         if mode%2 == 1:
-            print(f"{ansi_line1}{afficher_date()[0]} {afficher_date()[1]} {afficher_date()[2]}   \
-{afficher_heure()[1]} : {afficher_heure()[2]} : {afficher_heure()[3]}\033[u", end="", flush=True)
-        time.sleep(0.99)
+            print(f"{cursor_line_1}{calendar()[0]} {calendar()[1]} {calendar()[2]}   \
+{clock()[1]} : {clock()[2]} : {clock()[3]}{cursor_load}", end="", flush=True)
+        time.sleep(0.5)
         if stop == 1:
             time.sleep(1)
-            print(f"\033[H\033[2J", end="")
+            print(f"{cursor_heavycls}", end="")
             return
 
-def alerte():
-    global alarme
-    while True:
-        if alarme[0] != "__":
-            if afficher_heure()[int(mode%2)] == alarme[int(mode%2)] and afficher_heure()[2] == alarme[2] and afficher_heure()[3] == alarme[3]:
-                print(f"\033[s\033[H\033[1A\033[2KRINGGGGG\033[u", end="", flush=True)
-                time.sleep(5)
-                alarme = ["__","__","__","__","__"]
-                afficher_alarme()
-        if stop == 1 or alarme[0] == "__":
-            return
-        time.sleep(0.2)
-
-def terminal():
+def command_terminal():
     while True:
         global mode, stop
-        commande = input(f"\033[H\033[3B\033[0JCommande : ")
+        commande = input(f"{cursor_line_4}{cursor_lightcls}Commande : ")
         if commande == "mode":
             mode +=1
-            afficher_alarme()
             continue
         if commande == "alarme":
-            lancer_alarme()
-            threading.Thread(target=alerte).start()
+            set_alarm()
         if commande == "stop":
             stop +=1
             break
         else: continue
 
-def lancer_alarme():
-    if vérifier_alarme(int(mode%2)) == 0:
+def set_alarm():
+    if input_unit_alarm(int(mode%2)) == 0:
         return
-    vérifier_alarme(2)
-    vérifier_alarme(3)
+    input_unit_alarm(2)
+    input_unit_alarm(3)
     if mode%2 == 0:
-        print(f"\033[H\033[4B\033[2KEntrez {unité[4]}", end="")
+        print(f"{cursor_line_5}Entrez {unit[4]}", end="")
         while True:
-            alarme[4] = input(f"\033[H\033[3B\033[2KAlarme : {alarme[0]}H {alarme[2]}M {alarme[3]}S __\033[2D").strip(" ").upper()
-            if alarme[4] not in ("AM", "PM"):
+            alarm[4] = input(f"{cursor_line_4}Alarme : {alarm[0]}H {alarm[2]}M {alarm[3]}S __\033[2D").strip(" ").upper()
+            if alarm[4] not in ("AM", "PM"):
                 continue
             else: break
-        if alarme[4] == "PM" and str(alarme[0]) != "12":
-            alarme[1] = str(int(alarme[0])+12)
-        elif alarme[4] == "AM" and str(alarme[0]) == "12":
-            alarme[1] = "00"
-        else: alarme[1] = alarme[0]
+        if alarm[4] == "PM" and str(alarm[0]) != "12":
+            alarm[1] = str(int(alarm[0])+12)
+        elif alarm[4] == "AM" and str(alarm[0]) == "12":
+            alarm[1] = "00"
+        else: alarm[1] = alarm[0]
     if mode%2 == 1:
-        if alarme[1] == "00": alarme[0] = "12"; alarme[4] = "AM"
-        elif 12 < int(alarme[1]) < 22:
-            alarme[0] = "0"+str(int(alarme[1])-12)
-            alarme[4] = "PM"
-        elif 21 < int(alarme[1]):
-            alarme[0] = int(alarme[1])-12
-            alarme[4] = "PM"
+        if alarm[1] == "00": alarm[0] = "12"; alarm[4] = "AM"
+        elif 12 < int(alarm[1]) < 22:
+            alarm[0] = "0"+str(int(alarm[1])-12)
+            alarm[4] = "PM"
+        elif 21 < int(alarm[1]):
+            alarm[0] = int(alarm[1])-12
+            alarm[4] = "PM"
         else: 
-            alarme[0] = alarme[1]
-            alarme[4] = "AM"
-    print(f"\033[H\033[3B\033[0J", end="")
-    afficher_alarme()
+            alarm[0] = alarm[1]
+            alarm[4] = "AM"
+    print(f"{cursor_line_4}{cursor_lightcls}", end="")
 
-def vérifier_alarme(valeur):
-    global alarme, unité
-    print(f"\033[H\033[4B\033[2KEntrez {unité[valeur]}")
+def input_unit_alarm(valeur):
+    global alarm, unit
+    print(f"{cursor_line_5}Entrez {unit[valeur]}")
     while True:
-        alarme[valeur] = "__"
-        alarme[valeur] = input(f"\033[H\033[3B\033[2KAlarme : {alarme[int(mode%2)]}H {alarme[2]}M {alarme[3]}S\033[{unité_b[valeur]}D").strip("- ,.")
-        # if not alarme[0] and not alarme[1]:
-        #     choix = input(f"\033[2K\033[F\033[2KAnnuler l'alarme ? o/n \n").strip().lower()
+        alarm[valeur] = "__"
+        alarm[valeur] = input(f"{cursor_line_4}Alarme : {alarm[int(mode%2)]}H {alarm[2]}M {alarm[3]}S\033[{unit_b[valeur]}D").strip("- ,.")
+        # if not alarm[0] and not alarm[1]:
+        #     choix = input(f"\033[2K\033[F\033[2KAnnuler l'alarm ? o/n \n").strip().lower()
         #     if choix in ('o', 'oui'):
         #         print(f"\033[F\033[2K", end="")
         #         return 0
         #     else: continue
-        try: test = int(alarme[valeur])
+        try: test = int(alarm[valeur])
         except Exception: 
-            print("\033[H\033[5B\033[2K/!\ Entrez une valeur numérique sous la forme '00'", end="")
+            print(f"{cursor_line_6}/!\ Entrez une valeur numérique sous la forme '00'", end="")
             continue
-        if 1 == len(str(alarme[valeur])) or len(str(alarme[valeur])) >=3 :
-            print("\033[H\033[5B\033[2K/!\ Entrez une valeur constituée de 2 chiffres '00'", end="")
+        if 1 == len(str(alarm[valeur])) or len(str(alarm[valeur])) >=3 :
+            print(f"{cursor_line_6}/!\ Entrez une valeur constituée de 2 chiffres '00'", end="")
             continue
-        elif test >= unité_max[valeur]+1:
-            print(f"\033[H\033[5B\033[2K/!\ Entrez une valeur correcte à l'unité (entre 00 et {unité_max[valeur]})", end="")
+        elif test >= unit_max[valeur]+1:
+            print(f"{cursor_line_6}/!\ Entrez une valeur correcte à l'unité (entre 00 et {unit_max[valeur]})", end="")
             continue
-        elif len(str(alarme[valeur])) == 2:
-            print(f"\033[H\033[3B\033[0J", end="")
+        elif len(str(alarm[valeur])) == 2:
+            print(f"{cursor_line_4}{cursor_lightcls}", end="")
             return 1
 
 
-print(f"\033[H\033[2J", end="")
-print(f"\n\033[mAucune alarme définie.\033[25m", end="")
-threading.Thread(target=terminal).start()
-threading.Thread(target=affichage).start()
+print(f"{cursor_heavycls}", end="")
+threading.Thread(target=command_terminal).start()
+threading.Thread(target=display_clock).start()
+time.sleep(0.5)
+threading.Thread(target=display_alarm).start()
