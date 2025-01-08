f2fa5333bc0f58dd7195facbb515d24033c5ab00
Adeline <adeline.patenne@hotmail.fr>
2025-01-08 16:56:45 +0100

    feat : add class Time and Alarm
    not working, alarm class needs fixing

--- timeclass.py
+++ timeclass.py
@@ -12,6 +12,7 @@ class Time:
 
     def in_seconds(self):
         self.in_second = self.hour*3600 + self.minute*60 + self.second
+        return self.in_second
         #epoch 
 
     def increment_time(self):
@@ -28,7 +29,8 @@ class Time:
                 else:
                     self.hour = 0
 
-    def __str__ (self):
+    def display_time(self):
+        # self.change_format()
         if self.format == "24h":
             return f"{self.hour:02d} : {self.minute:02d} : {self.second:02d}"
         else:
@@ -42,8 +44,46 @@ class Time:
                     return f"{self.hour:02d} : {self.minute:02d} : {self.second:02d} PM"
                 else:
                     return f"{(self.hour - 12):02d} : {self.minute:02d} : {self.second:02d} PM"
+      
+
+    # def change_format(self, new_format):
+
+    #     if self.format == "12h" and new_format == "24h":
+    #         if self.hour < 12:
+    #             return f"{self.hour:02d} : {self.minute:02d} : {self.second:02d}"
+    #     elif self.format == "24h" and new_format == "12h":
+    #         if self.hour < 12:
+    #             if self.hour == 0:
+    #                 return f"12 : {self.minute:02d} : {self.second:02d} AM"
+    #             return f"{self.hour:02d} : {self.minute:02d} : {self.second:02d} AM"
+    #         else:
+    #             if self.hour == 12:
+    #                 return f"{self.hour:02d} : {self.minute:02d} : {self.second:02d} PM"
+    #             else:
+    #                 return f"{(self.hour - 12):02d} : {self.minute:02d} : {self.second:02d} PM"
 
 
+    def __str__ (self):
+        return self.display_time()
+ 
+class Alarm(Time):
+    max_display = 10 #10 seconds
 
+    def __init__(self, hour, minute, second, format, enabled=False):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+        self.format = format
+        self.enabled = enabled
+        self.in_seconds()
+    
+    def __str__ (self):
+        if self.enabled:
+            alarm_display = f" Alarm : {self.hour:02d} : {self.minute:02d} : {self.second:02d}"
+            return f"{alarm_display:>30}"
 
+        # if self.enabled:
+        #     return f" ; Alarm : {self.hour:02d} : {self.minute:02d} : {self.second:02d}" + self.display_alarm(self, clock)
+        # else:
+        #     return ""
 
