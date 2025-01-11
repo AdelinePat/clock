886291f9e207e4ef7f7360e47aaed3ee946ca53d
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-01-11 15:43:19 +0100

    fix mode not displaying or edit clock correctly and delete remaining test files

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import display
 from Clock_class import Clock
 from Alarm_class import Alarm
 
-def command_terminal(event, clock_time):
+def command_terminal(event, clock_time, alarm_time):
     while True:
         display.message_help()
         command = display.input_command()
@@ -17,29 +17,29 @@ def command_terminal(event, clock_time):
                 #    clock_time.clock = (clock_time.clock[0]-12, clock_time.clock[1], clock_time.clock[2])
                 #    clock_time.format = "AM"
                 continue
-            case "horloge":
+            case "horloge" | "h":
                 event.clear()
-                clock_time.clock = clock_time.change_clock()
+                clock_time.change_clock()
                 event.set()
                 continue
-            case "alarme":
+            case "alarme" | "a":
                 if not alarm_time:
                     alarm_time = Alarm((0,0,0),0,"", clock_time.mode)
-                    threading.Thread(target=alarm_time.display_alarm).start()
-                alarm_time.alarm = alarm_time.change_alarm()
+                    threading.Thread(target=alarm_time.display_alarm, args=(clock_time,)).start()
+                alarm_time.change_alarm()
                 continue
-            case "stop":
+            case "stop" | "s":
                 event.clear()
                 display.message_stop()
                 continue
-            case "demarrer":
+            case "demarrer" | "d":
                 event.set()
                 display.message_start()
                 continue
-            case "aide":
+            case "commandes" | "c":
                 display.message_help()
                 continue
-            case "quitter":
+            case "quitter" | "q":
                 display.message_byebye()
                 time.sleep(3)
                 break
@@ -48,16 +48,16 @@ def command_terminal(event, clock_time):
 
 def main():
     clock_time = Clock((0,0,0),0,"", False)
+    alarm_time = False
     event = threading.Event()
+    quit = False
 
     display.all_clear()
-
     display.message_first_time()
 
     clock_time.change_clock()
 
-    #TODO First time configuration
-    threading.Thread(target=command_terminal, args=(event, clock_time)).start()
+    threading.Thread(target=command_terminal, args=(event, clock_time, alarm_time)).start()
     event.set()
 
     threading.Thread(target=clock_time.display_clock, args=(event,)).start()   
