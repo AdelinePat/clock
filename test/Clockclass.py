2e6ba2dd12d36de8477f4e036977a9292851f3ba
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-01-11 11:13:10 +0100

    add some error message and delete some unused files. Also change how display works to show the clock

--- test/Clockclass.py
+++ test/Clockclass.py
@@ -1,6 +1,77 @@
+from datetime import datetime
+import time, display
+
 class Clock:
     def __init__(self, clock, ampm_hour, format, mode):
         self.clock = clock
         self.ampm_hour = ampm_hour
         self.format = format
         self.mode = False
+
+    def clock_ticking(self, clock_datetime):
+        clock_datetime = clock_datetime + datetime.timedelta(seconds=1)
+        self.clock = (int(clock_datetime.strftime('%H')), int(clock_datetime.strftime('%M')), int(clock_datetime.strftime('%S')))
+        
+        # %I = hour in 12h format
+        self.ampm_hour = int(clock_datetime.strftime('%I'))
+        # %p = AM/PM
+        self.format = clock_datetime.strftime('%p')
+
+    def display_clock(self, event, dic_cursor):
+        while True:
+            event.wait()
+            if self.mode == False:
+                clock_datetime = datetime.datetime(1970, 1, 1, self.clock[0], self.clock[1], self.clock[2])
+                # TODO display the tuple instead; change datetime only in change_clock
+                display.clock_24h(self)
+            else:
+                clock_datetime = datetime.datetime(1970, 1, 1, self.ampm_hour, self.clock[1], self.clock[2])
+                display.clock_12h(self)
+                
+            self.clock_ticking(clock_datetime)
+
+            time.sleep(1)
+
+    def change_clock(self):
+        while True:
+            clock_config = display.input_clock_config()
+
+            if clock_config == "automatique" or clock_config == "auto" or clock_config == "a":
+                current_time = datetime.now()
+                return (int(current_time("%H")), int(current_time("%M")), int(current_time("%S")))
+
+            elif clock_config == "manuel" or clock_config == "m":
+                while True:
+                    user_clock = display.input_user_clock()
+                        
+                    #Verify if it's a number
+                    try:
+                        test = int(user_clock)
+                    except Exception:
+                        display.error_NaN()
+                        continue
+
+                    #Verify if it's a valid hour with datetime error directly
+                    try:
+                        test = datetime.datetime(1970, 1, 1, user_clock[:2], user_clock[2:4], user_clock[4:])
+                    except Exception:
+                        display.error_invalid_time()
+                        continue
+
+                    #Verify if the input contain exactly 6 numbers
+                    if len(user_clock) != 6:
+                        display.error_number_length()
+                        continue
+
+                    if self.mode == True:
+                        user_format = display.input_user_format(user_clock)
+
+                        if user_format != "AM" and "PM":
+                            display.error_format()
+                            continue
+
+                    display.message_clock_valid()
+                    return (int(user_clock[0:2]), int(user_clock[2:4]), int(user_clock[4:]))        
+            
+            else:
+                continue
---
+++ test/display.py
@@ -0,0 +1,70 @@
+#TODO variable for each line (l) + the "clear line" separated + the "save cursor"
+
+#TODO display available commands
+
+#TODO center the display
+
+# ANSI codes
+dic_cursor = {
+    "delete_line_1-clock": "\033[s\033[H\033[2K",
+    "delete_line_2-alarm": "\033[s\033[H\033[1B\033[2K",
+    "delete_line_3-message": "\033[H\033[2B\033[2K",
+    "delete_line_4-command": "\033[H\033[3B\033[2K",
+    "delete_line_5-input": "\033[H\033[4B\033[2K",
+    "delete_line_6-error": "\033[H\033[5B\033[2K",
+
+    "cursor_position_load": "\033[u",
+
+    "cursor_move_left_2": "\033[2D",
+    "cursor_move_left_8": "\033[8D",
+
+    "delete_line": "\033[2K",
+    "delete_all_after_position": "\033[0J",
+    "delete_all": "\033[H\033[0J"
+}
+
+# Special actions
+def all_clear():
+    print(dic_cursor["delete_all"], end="")
+
+# Line 1 - Clock
+def clock_12h(clock_time):
+    print(f"{dic_cursor["delete_line_1-clock"]}{clock_time.ampm_hour}:{clock_time.clock[1]}:{clock_time.clock[2]} {clock_time.format}{dic_cursor["cursor_position_load"]}", end="", flush=True)
+
+def clock_24h(clock_time):
+    print(f"{dic_cursor["delete_line_1-clock"]}{clock_time.clock[0]}:{clock_time.clock[1]}:{clock_time.clock[2]}{dic_cursor["cursor_position_load"]}", end="", flush=True)
+
+# Line 3 - Message
+
+def message_clock_valid(dic_cursor):
+    print(f"{dic_cursor["delete_line_3-message"]}Horloge changé avec succes!{dic_cursor["cursor_position_load"]}", end="")
+
+def message_stop(dic_cursor):
+    print(f"{dic_cursor["delete_line_3-message"]}Horloge stoppé.", end="") #ZA WARUDO!!!
+
+def message_start(dic_cursor):
+    print(f"{dic_cursor["delete_line_3-message"]}Horloge redémarré.", end="") #TOKI WO UGOKIDASU.
+
+# Line 4 - Commands
+def input_command():
+    input(f"{dic_cursor["delete_line_4-command"]}{dic_cursor["delete_all_after_position"]}Commande : ")
+
+# Line 5 - Input
+def input_clock_config():
+    input(f"{dic_cursor["delete_line_5-input"]}{dic_cursor["delete_all_after_position"]}Configurer l'heure en: (M)anuel, (A)uto? ").lower()
+
+def input_user_clock():
+    input(f"{dic_cursor["delete_line_5-input"]}Veuillez entrer l'heure au format hh:mm:ss : ________{dic_cursor["cursor_move_left_8"]}").strip(" :.,/")
+
+def input_user_format(user_clock):
+    input(f"{dic_cursor["delete_line_5-input"]}Veuillez entrer AM ou PM : {user_clock[:2]}:{user_clock[2:4]}:{user_clock[4:]} __{dic_cursor["cursor_move_left_2"]}")
+# Line 6 - Errors
+def error_invalid_time():
+    print(f"{dic_cursor["delete_line_6-error"]}/!\Entrer une heure valide!")
+
+def error_NaN():
+    print(f"{dic_cursor["delete_line_6-error"]}/!\Enter uniquement des chiffres!")
+
+
+
+
