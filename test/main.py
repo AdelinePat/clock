a4ac1504ed47042214803af077e3bb4a7aa51a62
jeremy-gavend <jeremy.gavend@laplateforme.io>
2025-01-11 11:13:10 +0100

    add some error message and delete some unused files. Also change how display works to show the clock

--- test/main.py
+++ test/main.py
@@ -1,60 +1,53 @@
 import time, threading
-import alarm, clock, message
+import alarm, display
 from Clockclass import Clock
 
-clock_time = Clock((0,0,0),0,"", False)
-event = threading.Event()
-
-#TODO variable for each line (l) + the "clear line" separated + the "save cursor"
-# ANSI codes
-dic_cursor = {
-    "delete_line_1-clock": "\033[s\033[H\033[2K",
-    "delete_line_2-alarm": "\033[s\033[H\033[1B\033[2K",
-    "delete_line_3-message": "\033[H\033[2B\033[2K",
-    "delete_line_4-command": "\033[H\033[3B\033[2K",
-    "delete_line_5-info": "\033[H\033[4B\033[2K",
-    "delete_line_6-error": "\033[H\033[5B\033[2K",
-
-    "cursor_position_start": "\033[H",
-    "cursor_position_load": "\033[u",
-    "delete_line": "\033[2K",
-    "delete_all_after_position": "\033[0J",
-    "delete_all": "\033[H\033[0J"
-}
 
 def command_terminal(event, clock_time):
     while True:
-        command = input(f"{dic_cursor["delete_line_4-command"]}{dic_cursor["delete_all_after_position"]}Commande : ")
+        command = display.input_command()
         match command:
             case "mode":
-                print(f"{dic_cursor["delete_line_3-message"]}{clock_time.clock}")
                 clock_time.mode = not clock_time.mode
-                if clock_time.format == "AM":
-                    clock_time.clock = (clock_time.clock[0]+12, clock_time.clock[1], clock_time.clock[2])
-                    clock_time.format = "PM"
-                else:
-                    clock_time.clock = (clock_time.clock[0]-12, clock_time.clock[1], clock_time.clock[2])
-                    clock_time.format = "AM"
+                # if clock_time.format == "AM":
+                #    clock_time.clock = (clock_time.clock[0]+12, clock_time.clock[1], clock_time.clock[2])
+                #    clock_time.format = "PM"
+                # else:
+                #    clock_time.clock = (clock_time.clock[0]-12, clock_time.clock[1], clock_time.clock[2])
+                #    clock_time.format = "AM"
                 continue
             case "clock":
                 event.clear()
-                clock_time.clock = clock.change_clock(dic_cursor, clock_time)
+                clock_time.clock = clock_time.change_clock()
                 event.set()
                 continue
+            case "alarm":
+                pass
             case "stop":
                 event.clear()
-                message.stop(dic_cursor)
+                display.message_stop()
             case "start":
                 event.set()
-                message.start(dic_cursor)
+                display.message_start()
             case _:
                 continue
 
 def main():
-    print(dic_cursor["delete_all"], end="")
+    clock_time = Clock((0,0,0),0,"", False)
+    event = threading.Event()
+
+    display.all_clear()
+
+    #display.message_first_time()
+
+    clock_time.change_clock()
+
+    #TODO First time configuration
     threading.Thread(target=command_terminal, args=(event, clock_time)).start()
     event.set()
-    threading.Thread(target=clock.display_clock, args=(event, clock_time, dic_cursor)).start()
-    #threading.Thread(target=alarm.display_alarm).start()
+
+    threading.Thread(target=clock_time.display_clock, args=(event)).start()
+    #if alarm
+    #   threading.Thread(target=alarm.display_alarm).start()
 
 main()
